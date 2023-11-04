88a42387950da124231d9943cb1ea352cd394275
Ahmed Ahmed <afaraj@outlook.com>
2023-11-03 20:53:34 -0700

    verify base dir

--- backend/settings.py
+++ backend/settings.py
@@ -18,6 +18,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(f"BASE_DIR: {BASE_DIR}")
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
