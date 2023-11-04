697506e8064d66b3ba6d694f188c3f8716d3e9af
Ahmed Ahmed <afaraj@outlook.com>
2023-11-03 20:27:53 -0700

    fixes

--- backend/settings.py
+++ backend/settings.py
@@ -10,10 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import django_heroku
+import dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,5 +137,5 @@ CORS_ORIGIN_WHITELIST = [
     'https://localhost:3000',
 ]
 
-import django_heroku
 django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
